1ee1ec99ac6ec2b6a42f86a36d70e8bba17466f9
Ezee1015 <routerciscoanuitti@gmail.com>
2022-11-14 16:18:39 -0300

    Agrega la versión gráfica del programa

---
+++ Grafico/OrganizadorCarpeta/src/organizadorcarpeta/Dias.java
@@ -0,0 +1,20 @@
+package organizadorcarpeta;
+
+import java.util.ArrayList;
+
+public class Dias {
+    private ArrayList<String> materiasDelDia;
+
+    public Dias(ArrayList<String> materiasDelDia) {
+        this.materiasDelDia = materiasDelDia;
+    }
+
+    public ArrayList<String> getMateriasDelDia() {
+        return materiasDelDia;
+    }
+
+    public void setMateriasDelDia(ArrayList<String> materiasDelDia) {
+        this.materiasDelDia = materiasDelDia;
+    }
+    
+}
---
+++ Grafico/OrganizadorCarpeta/src/organizadorcarpeta/OrganizadorCarpeta.java
@@ -0,0 +1,10 @@
+package organizadorcarpeta;
+
+public class OrganizadorCarpeta {
+
+    public static void main(String[] args) {
+        MenuPrincipal temp = new MenuPrincipal();
+        temp.main(null);
+    }
+    
+}
